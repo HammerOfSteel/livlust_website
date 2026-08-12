48cf2d0d4c426d4cf1e5bf9e628543621e43336a
HammerOfSteel <93763165+HammerOfSteel@users.noreply.github.com>
2026-08-12 09:44:53 +0200

    feat(frontend): add shared Post type

---
+++ frontend/src/types/post.ts
@@ -0,0 +1,13 @@
+export interface Post {
+  id: number;
+  slug: string;
+  language: 'sv' | 'en';
+  status: 'published' | 'draft';
+  title: string;
+  excerpt: string | null;
+  body: string | null;
+  published_at: string;
+  image: { id: string } | null;
+  image_key: string | null;
+  image_alt: string | null;
+}
